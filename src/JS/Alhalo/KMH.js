fe0f7ad6d99ca40956740193d29a27038a447c3d
rbrt1022 <rbrt1022.it@outlook.hu>
2023-07-01 10:05:50 +0200

    07.01

---
+++ src/JS/Alhalo/KMH.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Alhalo from "../Tartalom/Alhalo";
+
+const KMH = () => {
+    let alhsz = 1
+
+    function hozzaad(){
+        alhsz++
+        console.log(alhsz)
+        const hozzaados = document.getElementsByClassName("szerkesztos")
+        
+    }
+
+    function KF(){
+
+
+    }
+    
+    return(
+        <div id="Kulonbozo">
+            <Alhalo />
+                <div class="enyhezold">
+                    <h2>Felbontandó hálózat címe: <input className="beiros" id="halo" type="text" placeholder="Írjad bé!" /></h2>
+                    <div id="alhcuccos">
+                        <div class="csutkafeher">
+                            <h2><label for="alhm">1. alhálózat -  Állomásoknak szánt IP címek száma: </label>
+                                <select id="alhm" name="alhalomeret">
+                                    <option value="2">2</option>
+                                    <option value="6">6</option>
+                                    <option value="14">14</option>
+                                    <option value="30">30</option>
+                                    <option value="62">62</option>
+                                    <option value="126">126</option>
+                                    <option value="254">254</option>
+                                </select>
+                            </h2>
+                        </div>
+                    </div>
+                    <div id="gomb" className="csutkafeher" onClick={hozzaad}><h2 id="plusz">+</h2></div>
+
+                    <button className="gomb" onClick={KF}>Szamolj</button>
+                </div>
+                <div id="tablazat">{/*tablazat*/}</div>
+            </div>
+    )
+}
+
+export default KMH;
